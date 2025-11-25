fd9600c526c7be1b155615e80a701bf233ccbd94
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-25 14:07:59 +0800

    🎉 init: app schema (user & transaction)

---
+++ server/models/enum.go
@@ -0,0 +1,15 @@
+package models
+
+type Role string
+
+const (
+	Admin Role = "ADMIN"
+	Public Role = "PUBLIC"	
+)
+
+type TransactionCategory string
+
+const (
+	Debit TransactionCategory = "DEBIT"
+	Credit TransactionCategory = "CREDIT"
+)
---
+++ server/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID uuid.UUID `gorm:"primaryKey;type:uuid" json:"id"`
+	Username string	`gorm:"type:varchar(100);not null" json:"username"`
+	Email string `gorm:"type:varchar(100); not null" json:"email"`
+	Password string `gorm:"not null" json:"password"`
+	Bio	string `gorm:"type:text" json:"bio"`
+	Role Role `gorm:"type:varchar(20)" json:"role"`
+
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+
+	Transactions []Transaction `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+}
