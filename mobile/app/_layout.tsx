5fa406e290b9131b01a7e07c68b83e962063e79f
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-26 15:38:25 +0800

    🎉 init: set up root & auth layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,7 +2,7 @@ import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
 
-export default function RootLayout() {
+export default function Layout() {
   return (
     <ScreenWrapper>
       <Slot />
