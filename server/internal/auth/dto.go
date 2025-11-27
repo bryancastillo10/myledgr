685895ae0c96144258c55d4a395fc3a53b07862d
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-27 14:41:49 +0800

    🌐api: auth feature dto

---
+++ server/internal/auth/dto.go
@@ -0,0 +1,18 @@
+package auth
+
+type SignUpRequest struct {
+	Username string `json:"id"`	
+	Email string `json:"email"`
+	Password string `json:"password"`
+	ConfirmPassword string `json:"confirmPassword"`
+}
+
+type SignInRequest struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWTAuthResponse struct {
+	ID string	`json:"id"`
+	Role string	`json:"role"`
+}
