052d4673c2a951ff23fd6d5d6db20da3bbb1b5f5
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-01 13:54:55 +0800

    🎉init: profile screen

---
+++ mobile/app/(profile)/edit.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+import ScreenWrapper from "@/components/layout/ScreenWrapper";
+export default function EditProfile() {
+  return (
+    <ScreenWrapper>
+      <View>
+        <Text>Edit Profile Screen</Text>
+      </View>
+    </ScreenWrapper>
+  );
+}
