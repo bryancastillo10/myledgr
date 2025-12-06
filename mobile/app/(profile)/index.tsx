6b69fc0a9067d0d483d20ce22fc780fc747629b2
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-12-06 22:14:43 +0800

    🧹 chore: add filler button for temporary routing

--- mobile/app/(profile)/index.tsx
+++ mobile/app/(profile)/index.tsx
@@ -1,11 +1,21 @@
 import { View, Text } from "react-native";
+import { useRouter } from "expo-router";
 
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
+import Button from "@/components/ui/Button";
+
 export default function ViewProfile() {
+  const router = useRouter();
+
   return (
     <ScreenWrapper>
       <View>
         <Text>View Profile Screen</Text>
+
+        <Button
+          textButton="Go to Home"
+          onPress={() => router.push("/welcome")}
+        />
       </View>
     </ScreenWrapper>
   );
