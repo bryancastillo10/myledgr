4dd71401c2570b0330336f4a2fd3b792953716c4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-08 10:36:56 +0800

    ✨feature: transaction header access to create transaction screen

--- mobile/app/(transaction)/create.tsx
+++ mobile/app/(transaction)/create.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
 
 export default function CreateTransaction() {
+  const router = useRouter();
+
   return (
     <ScreenWrapper>
       <View>
         <Text>Create Transaction Screen</Text>
+
+        <Pressable onPress={() => router.back()}>
+          <Text>Get Back</Text>
+        </Pressable>
       </View>
     </ScreenWrapper>
   );
--- mobile/features/transaction/components/TransactionHeader.tsx
+++ mobile/features/transaction/components/TransactionHeader.tsx
@@ -1,13 +1,51 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { COLORS } from "@/constants/colors";
+
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 
 const TransactionHeader = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Recent Transactions</Text>
+    <View style={styles.header}>
+      <Text style={styles.recentTransactionText}>Recent Transactions</Text>
+
+      <TouchableOpacity
+        onPress={() => router.push("/(transaction)/create")}
+        style={styles.addButton}
+      >
+        <Ionicons name="add-circle" size={18} color={COLORS.white} />
+        <Text style={styles.addButtonText}>Add</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default TransactionHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 6,
+  },
+  recentTransactionText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: COLORS.text,
+  },
+  addButton: {
+    flexDirection: "row",
+    gap: 6,
+    backgroundColor: COLORS.primary,
+    borderRadius: 14,
+    padding: 8,
+  },
+  addButtonText: {
+    fontWeight: "600",
+    letterSpacing: 0.75,
+    color: COLORS.white,
+  },
+});
