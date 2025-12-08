67a1e741ecf918347925be04ee89fc6ab618a5bd
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-08 08:56:52 +0800

    🌐model: added more fields to User & Transaction

--- server/models/user.go
+++ server/models/user.go
@@ -12,8 +12,10 @@ type User struct {
 	Email    string    `gorm:"type:varchar(100); not null" json:"email"`
 	Password string    `gorm:"not null" json:"password"`
 	Bio      *string   `gorm:"type:text" json:"bio,omitempty"`
+	Address  *string   `gorm:"type:text" json:"address,omitempty"`
+	Theme	 *string   `gorm:"type:text" json:"theme,omitempty"`
 	Role     Role      `gorm:"type:varchar(20)" json:"role"`
-
+    
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
 
