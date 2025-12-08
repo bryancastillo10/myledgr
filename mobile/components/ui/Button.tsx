9b3b739993bb8ccd33b57dc39de5cdd5d0f076e9
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-08 15:38:20 +0800

    🧹chore: improve button with optional icon props

--- mobile/components/ui/Button.tsx
+++ mobile/components/ui/Button.tsx
@@ -1,14 +1,24 @@
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { Ionicons as IconType } from "@expo/vector-icons";
+
 import { COLORS } from "@/constants/colors";
 
 interface ButtonProps {
   onPress: () => void;
+  icon?: keyof typeof IconType.glyphMap;
   textButton: string;
 }
 
-const Button = ({ onPress, textButton }: ButtonProps) => {
+const Button = ({ onPress, textButton, icon }: ButtonProps) => {
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={onPress}
+      activeOpacity={0.85}
+    >
+      {icon && <Ionicons name={icon} size={18} color={COLORS.white} />}
       <Text style={styles.buttonText}>{textButton}</Text>
     </TouchableOpacity>
   );
@@ -19,16 +29,29 @@ export default Button;
 const styles = StyleSheet.create({
   button: {
     backgroundColor: COLORS.primary,
-    borderRadius: 12,
-    padding: 16,
+    flexDirection: "row",
+    borderRadius: 16,
+    padding: 18,
+    justifyContent: "flex-start",
     alignItems: "center",
     marginTop: 10,
     marginBottom: 20,
+    shadowColor: COLORS.primary,
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 12,
+    elevation: 8,
+    borderWidth: 0.5,
+    borderBottomWidth: 0,
+    gap: 8,
   },
   buttonText: {
     color: COLORS.white,
-    fontSize: 18,
-    fontWeight: "600",
-    letterSpacing: 1.25,
+    fontSize: 17,
+    fontWeight: "700",
+    letterSpacing: 0.8,
   },
 });
