62baa3b66c12eeaff4d447fcba81ac1c42b21f2b
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-11 13:38:38 +0800

    🧩component: Select

--- mobile/app/(transaction)/create.tsx
+++ mobile/app/(transaction)/create.tsx
@@ -1,15 +1,31 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { useState } from "react";
 import { useRouter } from "expo-router";
 
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
 
+import Select from "@/components/ui/Select";
+import { currencySymbols } from "@/constants/currency";
+
 export default function CreateTransaction() {
   const router = useRouter();
 
+  const [category, setCategory] = useState<string | null>(null);
+
+  const categoryOptions = [
+    { label: "Income", value: "CREDIT" },
+    { label: "Expense", value: "DEBIT" },
+  ];
+
   return (
     <ScreenWrapper>
-      <View>
-        <Text>Create Transaction Screen</Text>
+      <View style={styles.container}>
+        <Select
+          label="Select Category"
+          options={categoryOptions}
+          value={category}
+          onChange={setCategory}
+        />
 
         <Pressable onPress={() => router.back()}>
           <Text>Get Back</Text>
@@ -18,3 +34,10 @@ export default function CreateTransaction() {
     </ScreenWrapper>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 12,
+    paddingHorizontal: 16,
+  },
+});
