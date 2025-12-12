0ab1d5113ba9dd4041b02b89b9a8bd0184e1b7a2
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-12 09:16:44 +0800

    🧩 component: create transaction screen (static)

--- mobile/app/(transaction)/create.tsx
+++ mobile/app/(transaction)/create.tsx
@@ -1,40 +1,48 @@
-import { View, StyleSheet } from "react-native";
-import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { categoryIcons } from "@/constants/icons";
 
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
-
-import Select from "@/components/ui/Select";
-
-import RadioButton from "@/components/ui/RadioButton";
 import ScreenHeader from "@/components/layout/ScreenHeader";
 
+import Input from "@/components/ui/Input";
+import Select from "@/components/ui/Select";
+import RadioButton from "@/components/ui/RadioButton";
+import Button from "@/components/ui/Button";
+
 export default function CreateTransaction() {
-  const [category, setCategory] = useState<string | null>(null);
-  const [radioSelected, setRadioSelected] = useState<boolean>(false);
-
-  const categoryOptions = [
-    { label: "Income", value: "CREDIT" },
-    { label: "Expense", value: "DEBIT" },
-  ];
-
   return (
     <ScreenWrapper>
       <ScreenHeader text="Add Transaction" />
-
       <View style={styles.container}>
+        <Input placeholder="Title" value="" onChange={() => {}} />
+
+        <Input isNumeric placeholder="Amount" value="" onChange={() => {}} />
+
         <Select
+          options={categoryIcons}
           label="Select Category"
-          options={categoryOptions}
-          value={category}
-          onChange={setCategory}
+          value=""
+          onChange={() => {}}
         />
 
-        <RadioButton
-          isSelected={radioSelected}
-          label="Test Radio Button"
-          icon="beaker"
-          onSelect={() => setRadioSelected(!radioSelected)}
-        />
+        <View style={styles.category}>
+          <Text style={styles.categoryText}>Transaction Type</Text>
+          <RadioButton
+            icon="arrow-up-circle"
+            isSelected
+            label="Debit"
+            onSelect={() => {}}
+          />
+          <RadioButton
+            icon="arrow-down-circle"
+            isSelected={false}
+            label="Credit"
+            onSelect={() => {}}
+          />
+        </View>
+        <View>
+          <Button textButton="Add Transaction" onPress={() => {}} />
+        </View>
       </View>
     </ScreenWrapper>
   );
@@ -44,5 +52,15 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 12,
     paddingHorizontal: 16,
+  },
+  category: {
+    marginVertical: 12,
+  },
+  categoryText: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  submit: {
+    marginVertical: 10,
   },
 });
