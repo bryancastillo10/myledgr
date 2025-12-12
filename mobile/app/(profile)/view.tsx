ab72e63366c756422fdec6d984fabaa490d84564
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-12 16:12:53 +0800

    🧩 component: EditProfileModal

--- mobile/app/(profile)/view.tsx
+++ mobile/app/(profile)/view.tsx
@@ -9,14 +9,14 @@ import { useAuthStore } from "@/lib/zustand/user";
 import { useModalStore } from "@/lib/zustand/modal";
 
 import ProfileRow from "@/features/user/components/ProfileRow";
+import EditProfileModal from "@/features/user/components/EditProfileModal";
 import Button from "@/components/ui/Button";
-import Modal from "@/components/ui/Modal";
 
 import { formatStringRender, formatDate } from "@/features/user/utils";
 
 export default function ViewProfile() {
   const user = useAuthStore((state) => state.user);
-  const { isOpen, modalType, setOpenModal, setCloseModal } = useModalStore();
+  const { setOpenModal } = useModalStore();
 
   if (!user) return <Redirect href="/welcome" />;
 
@@ -68,22 +68,7 @@ export default function ViewProfile() {
             canEdit={false}
           />
 
-          {isOpen && modalType === "username" && (
-            <Modal
-              isOpen={isOpen}
-              textHeader="Edit User Profile"
-              withHeader
-              body={
-                <View>
-                  <Text>Test</Text>
-                </View>
-              }
-              actionLeft={setCloseModal}
-              actionRight={setCloseModal}
-              actionRightLabel="Save"
-              actionLeftLabel="Close"
-            />
-          )}
+          <EditProfileModal user={user} />
         </View>
 
         <View style={[styles.content, { marginVertical: 14 }]}>
