6ccefd84d0be4794c6a5ff2eadf7073167a31fd0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-15 08:45:38 +0800

    📝constants: currency & theme options

--- mobile/constants/currency.ts
+++ mobile/constants/currency.ts
@@ -9,3 +9,42 @@ export const currencySymbols = {
   INR: "₹", // Indian Rupee
   AUD: "$", // Australian Dollar
 };
+
+export const currencyOptions = [
+  {
+    label: "USD ($)",
+    value: "USD",
+  },
+  {
+    label: "CAD ($)",
+    value: "CAD",
+  },
+  {
+    label: "EUR (€)",
+    value: "EUR",
+  },
+  {
+    label: "GBP (£)",
+    value: "GBP",
+  },
+  {
+    label: "JPY (¥)",
+    value: "JPY",
+  },
+  {
+    label: "PHP (₱)",
+    value: "PHP",
+  },
+  {
+    label: "TWD (NT$)",
+    value: "TWD",
+  },
+  {
+    label: "INR (₹)",
+    value: "INR",
+  },
+  {
+    label: "AUD ($)",
+    value: "AUD",
+  },
+];
