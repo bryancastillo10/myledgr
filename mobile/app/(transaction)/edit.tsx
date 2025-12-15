19ed046ef5bae47c4ef43e8e35f69dfc13678c87
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-15 15:18:52 +0800

    🎉init: edit transaction screen header

--- mobile/app/(transaction)/edit.tsx
+++ mobile/app/(transaction)/edit.tsx
@@ -2,18 +2,16 @@ import { View, Text, Pressable } from "react-native";
 import { useRouter } from "expo-router";
 
 import ScreenWrapper from "@/components/layout/ScreenWrapper";
+import ScreenHeader from "@/components/layout/ScreenHeader";
 
 export default function EditTransaction() {
   const router = useRouter();
 
   return (
     <ScreenWrapper>
+      <ScreenHeader text="Edit Transaction" />
       <View>
-        <Text>Edit Transaction Screen</Text>
-
-        <Pressable onPress={() => router.push("/(root)")}>
-          <Text>Get Back</Text>
-        </Pressable>
+        <Text>Edit Transaction Here</Text>
       </View>
     </ScreenWrapper>
   );
