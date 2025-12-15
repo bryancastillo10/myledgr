1bbc2347049bda2f7340b6e70e2b070991f774f0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-15 13:19:31 +0800

    ✨feature: delete user hook and modal UI

--- mobile/features/user/components/DeleteProfileModal.tsx
+++ mobile/features/user/components/DeleteProfileModal.tsx
@@ -1,19 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import Modal from "@/components/ui/Modal";
+import Input from "@/components/ui/Input";
 
 import { useModalStore } from "@/lib/zustand/modal";
+import useDeleteProfile from "@/features/user/hooks/useDeleteProfile";
+import useColor from "@/lib/providers/useColor";
 
 interface DeleteProfileModalProps {
   email: string;
 }
 
 const DeleteProfileModal = ({ email }: DeleteProfileModalProps) => {
+  const { COLORS } = useColor();
   const { isOpen, modalType, setCloseModal } = useModalStore();
 
+  const { typedEmail, loading, onChangeEmail, handleSubmit } =
+    useDeleteProfile();
+
   const modalBody = (
-    <View>
-      <Text>{email}</Text>
+    <View style={styles.container}>
+      <Text style={[styles.caption, { color: COLORS.text }]}>
+        Are you sure you want to delete your account? To confirm deletion,
+        please retype your email address.
+      </Text>
+      <Text style={[styles.emailText, { color: COLORS.primary }]}>{email}</Text>
+      <Input
+        placeholder="Type your email"
+        value={typedEmail}
+        onChange={onChangeEmail}
+      />
     </View>
   );
 
@@ -22,11 +38,13 @@ const DeleteProfileModal = ({ email }: DeleteProfileModalProps) => {
     modalType === "delete_account" && (
       <Modal
         isOpen={isOpen}
+        height="42%"
         textHeader="Confirm Delete Account"
         withHeader
         body={modalBody}
+        loading={loading}
         actionLeft={setCloseModal}
-        actionRight={setCloseModal}
+        actionRight={handleSubmit}
         actionRightLabel="Delete"
         actionLeftLabel="Close"
       />
@@ -35,3 +53,21 @@ const DeleteProfileModal = ({ email }: DeleteProfileModalProps) => {
 };
 
 export default DeleteProfileModal;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 4,
+  },
+  caption: {
+    textAlign: "justify",
+    fontSize: 16,
+    marginVertical: 4,
+  },
+  emailText: {
+    fontSize: 14,
+    textAlign: "right",
+    fontWeight: "600",
+    letterSpacing: 0.6,
+    marginBottom: 8,
+  },
+});
