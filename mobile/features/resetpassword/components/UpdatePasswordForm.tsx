055dc74bcdbc453b1bb4a2f07f43ec1cd210d71b
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-17 11:00:41 +0800

    🎉init: reset password screen steps render logic

---
+++ mobile/features/resetpassword/components/UpdatePasswordForm.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const UpdatePasswordForm = () => {
+  return (
+    <View>
+      <Text>Update Password Here</Text>
+    </View>
+  );
+};
+
+export default UpdatePasswordForm;
