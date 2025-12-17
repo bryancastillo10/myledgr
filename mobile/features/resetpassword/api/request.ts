0007c8e152fe91aa94f93d5d373ae9bcbeb224a3
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-17 09:44:14 +0800

    🌐api: initial client request for reset password api

---
+++ mobile/features/resetpassword/api/request.ts
@@ -0,0 +1,29 @@
+import { apiRequest } from "@/lib/api-client/client";
+
+import {
+  ResetPasswordRequest,
+  ResetPasswordStepResponse,
+  UpdatePasswordRequest,
+  UpdatePasswordResponse,
+  VerifyCodeRequest,
+} from "@/features/resetpassword/api/interface";
+
+const baseUrl = "reset-password";
+
+export const resetPasswordApi = {
+  requestReset: (body: ResetPasswordRequest) =>
+    apiRequest<ResetPasswordStepResponse>(`${baseUrl}/reset`, {
+      method: "POST",
+      body: JSON.stringify(body),
+    }),
+  verifyCode: (body: VerifyCodeRequest) =>
+    apiRequest<ResetPasswordStepResponse>(`${baseUrl}/verify-code`, {
+      method: "POST",
+      body: JSON.stringify(body),
+    }),
+  updatePassword: (body: UpdatePasswordRequest) =>
+    apiRequest<UpdatePasswordResponse>(`${baseUrl}/update`, {
+      method: "PUT",
+      body: JSON.stringify(body),
+    }),
+};
